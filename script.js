4739547708885e15baf416270e72ab2fd5571c4f
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-23 12:22:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let squares = document.querySelectorAll(".square");
+for ( let i = 0; i < squares.length; i++ ){
+	squares[i].addEventListener("mouseenter", onMouseEnter);
+	squares[i].addEventListener("mouseleave", onMouseLeave);
+}
+
+function onMouseEnter(event){
+	for ( let i = 0; i < squares.length; i++ ){
+	if(squares[i] != event.target){
+		squares[i].style.backgroundColor = "#6F4E37";
+	}
+}
+}
+
+function onMouseLeave() {
+	for ( let i = 0; i < squares.length; i++ ){
+		squares[i].style.backgroundColor = "#E6E6FA";
+}
+}
