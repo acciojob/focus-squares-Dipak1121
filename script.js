ded3c7ac06e305dda0d3c575388f091b3f5d588f
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-23 13:01:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 //your JS code here. If required.
-let squares = document.querySelectorAll(".square");
-for ( let i = 0; i < squares.length; i++ ){
-	squares[i].addEventListener("mouseenter", onMouseEnter);
-	squares[i].addEventListener("mouseleave", onMouseLeave);
-}
 
-function onMouseEnter(event){
-	for ( let i = 0; i < squares.length; i++ ){
-	if(squares[i] != event.target){
-		squares[i].style.backgroundColor = "rgb(111, 78, 55)";
-	}
-}
-}
 
-function onMouseLeave() {
-	for ( let i = 0; i < squares.length; i++ ){
-		squares[i].style.backgroundColor = "#E6E6FA";
-}
-}
+// let squares = document.querySelectorAll(".square");
+// for ( let i = 0; i < squares.length; i++ ){
+// 	squares[i].addEventListener("mouseenter", onMouseEnter);
+// 	squares[i].addEventListener("mouseleave", onMouseLeave);
+// }
+
+// function onMouseEnter(event){
+// 	for ( let i = 0; i < squares.length; i++ ){
+// 	if(squares[i] != event.target){
+// 		squares[i].style.backgroundColor = "rgb(111, 78, 55)";
+// 	}
+// }
+// }
+
+// function onMouseLeave() {
+// 	for ( let i = 0; i < squares.length; i++ ){
+// 		squares[i].style.backgroundColor = "#E6E6FA";
+// }
+// }
+
+const squares = document.querySelectorAll('.square');
+
+  squares.forEach(square => {
+    square.addEventListener('mouseover', () => {
+      squares.forEach(otherSquare => {
+        if (otherSquare !== square) {
+          otherSquare.classList.add('coffee');
+        }
+      });
+    });
+
+    square.addEventListener('mouseout', () => {
+      squares.forEach(otherSquare => {
+        if (otherSquare !== square) {
+          otherSquare.classList.remove('coffee');
+        }
+      });
+    });
+  });
